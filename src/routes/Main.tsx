49e744c91ca92ab4b38fb7a7b7845b1edc223ad9
Censuradho <gustavoleiteoliveira800@gmail.com>
2022-05-29 15:22:42 -0300

    refac: remove animation

--- src/routes/Main.tsx
+++ src/routes/Main.tsx
@@ -2,14 +2,10 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { RootMainParamList } from './types'
 
 import * as Pages from 'src/pages/private'
-import { useTheme } from 'src/hooks'
-import { ButtonIcon } from 'src/components'
 
 const Stack = createNativeStackNavigator<RootMainParamList>()
 
 export function Main () {
-  const { colors } = useTheme()
-  
   return (
     <Stack.Navigator
       screenOptions={{
@@ -22,12 +18,15 @@ export function Main () {
       />
       <Stack.Screen 
         name="WEEK" 
-        component={Pages.Week} 
+        component={Pages.Week}
 
       />
       <Stack.Screen 
         name="WORKOUT" 
         component={Pages.Workout}
+        options={{
+          animation: 'slide_from_right'
+        }}
 
       />
     </Stack.Navigator>
